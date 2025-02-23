470197bea6be66cc62c71c07d662618c09b8de9e
harshilthakkar2393 <harshilthakkar@protonmail.com>
2025-02-22 22:31:58 -0500

    big fix in db.ts , addded schema back to drizzle

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,17 +1,8 @@
-// import "dotenv/config";
-// import { drizzle } from "drizzle-orm/bun-sql";
-// import { neon, neonConfig } from '@neondatabase/serverless';
-
-// import ws from 'ws';
-// neonConfig.webSocketConstructor = ws;
-
-// const client = neon(process.env.DATABASE_URL!);
-// export const db = drizzle(client, { schema: UserMessages });
-
+import * as schema from "./schema"
 
 import { drizzle } from "drizzle-orm/bun-sql";
 import { SQL } from "bun";
 
 const client = new SQL(process.env.DATABASE_URL!);
-export const db = drizzle({ client });
+export const db = drizzle({ client,schema:schema });
 
