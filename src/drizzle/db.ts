95da13ebd55f2815b50e4d57daafd23ad71afa44
Udip <udipmandora42@gmail.com>
2025-02-22 22:22:04 -0500

    made changes into db.ts file and put original back

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,17 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import { drizzle } from "drizzle-orm/bun-sql";
+// import { neon, neonConfig } from '@neondatabase/serverless';
+
+// import ws from 'ws';
+// neonConfig.webSocketConstructor = ws;
+
+// const client = neon(process.env.DATABASE_URL!);
+// export const db = drizzle(client, { schema: UserMessages });
+
+
 import { drizzle } from "drizzle-orm/bun-sql";
-import { neon, neonConfig } from '@neondatabase/serverless';
+import { SQL } from "bun";
 
-import ws from 'ws';
-neonConfig.webSocketConstructor = ws;
+const client = new SQL(process.env.DATABASE_URL!);
+export const db = drizzle({ client });
 
-const sql = neon(process.env.DATABASE_URL!);
