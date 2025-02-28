e7c31d2cc7e6d2eff3c9b584dbadd3fa8baf0a5a
harshilthakkar2393 <harshilthakkar@protonmail.com>
2025-02-27 21:06:58 -0500

    added title in create question

--- src/data-access/questions.ts
+++ src/data-access/questions.ts
@@ -5,43 +5,60 @@ import { eq } from "drizzle-orm";
 /**
  * Get a question form database by id.
  * @param {string} id of the question
- * 
+ *
  * @returns Question object or undefined
  */
-export async function quesionGetOneById(id: string): Promise<{
-    id: string;
-    content: string | null;
-    userId: string;
-    createdAt: Date | null;
-    updatedAt: Date | null;
-} | undefined> {
-    const question = await db.query.QuestionTable.findFirst({ where: eq(QuestionTable.id, id) });
-    return question
+export async function quesionGetOneById(id: string): Promise<
+  | {
+      id: string;
+      content: string | null;
+      userId: string;
+      createdAt: Date | null;
+      updatedAt: Date | null;
+    }
+  | undefined
+> {
+  const question = await db.query.QuestionTable.findFirst({
+    where: eq(QuestionTable.id, id),
+  });
+  return question;
 }
 
 /**
  * Get a question from database by userId.
  * @param {string} userId of the question
- * 
+ *
  * @returns Question object or undefined
  */
-export async function questionGetByOneUserId(userId: string): Promise<{
-    id: string;
-    content: string | null;
-    userId: string;
-    createdAt: Date | null;
-    updatedAt: Date | null;
-} | undefined> {
-    const question = await db.query.QuestionTable.findFirst({ where: eq(QuestionTable.userId, userId) })
-    return question
+export async function questionGetByOneUserId(userId: string): Promise<
+  | {
+      id: string;
+      content: string | null;
+      userId: string;
+      createdAt: Date | null;
+      updatedAt: Date | null;
+    }
+  | undefined
+> {
+  const question = await db.query.QuestionTable.findFirst({
+    where: eq(QuestionTable.userId, userId),
+  });
+  return question;
 }
 
 /**
  * Create a new question in database
  * @returns {string} id of a newly created question.
- * 
+ *
  */
-export async function questionCreateOne(data: { content: string, userId: string }): Promise<{ id: string }> {
-    const [question] = await db.insert(QuestionTable).values(data).returning({ id: QuestionTable.id })
-    return question;
+export async function questionCreateOne(data: {
+  content: string;
+  title: string;
+  userId: string;
+}): Promise<{ id: string }> {
+  const [question] = await db
+    .insert(QuestionTable)
+    .values(data)
+    .returning({ id: QuestionTable.id });
+  return question;
 }
