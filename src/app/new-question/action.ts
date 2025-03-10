f6f4177ba1cab6d97582321366be8e366a5f9fe6
Udip <udipmandora42@gmail.com>
2025-03-10 18:34:57 -0400

    Added logic into action.ts file

--- src/app/new-question/action.ts
+++ src/app/new-question/action.ts
@@ -1,16 +1,19 @@
 import { QuestionFormData } from "./form";
 import { createNewQuestion } from "@/use-cases/question-service";
 
-export async function createQuestion(data:QuestionFormData)
-{
+export async function createQuestion(data: QuestionFormData) {
     // validate data 
+    if (!data.title || !data.description) {
+        throw new Error("Title and content are required.");
+    }
 
     // pass that data to this function\
-
-    try{
-        const result = await createNewQuestion();
-    }catch(error)
-    {
-        // handle error here ;
+    try {
+        const result = await createNewQuestion(data.title, data.description);
+        return result;
+    } catch (error) {
+        // handle error here
+        console.error("Error creating question: ", error);
+        throw new Error("Failed to create question");
     }
 }
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,7 @@
 import * as schema from "./schema"
-
 import { drizzle } from "drizzle-orm/bun-sql";
 import { SQL } from "bun";
 
 const client = new SQL(process.env.DATABASE_URL!);
-export const db = drizzle({ client,schema:schema });
+export const db = drizzle({ client, schema: schema });
 
