55b927f559dc2c7cde5d26dd2b0e2726295d350d
Udip <udipmandora42@gmail.com>
2025-03-17 22:21:32 -0400

    answer form created

--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -1,12 +1,8 @@
 import QuestionCard from "@/components/Questions/QuestionCard";
 import { getQuestionById } from "@/use-cases/question-service";
 import { notFound } from "next/navigation";
-<<<<<<< HEAD
-import { AnswerForm } from "@/app/new-question/answerForm";
-=======
 import AnswerQuestion from "./AnswerQuestion/AnswerQuestion";
 
->>>>>>> 4863f1c2e102620673c533f5b2f37d3b384ccbe5
 export default async function Page({
   params,
 }: {
@@ -20,13 +16,7 @@ export default async function Page({
   return (
     <div className="flex flex-col gap-2 w-full p-12">
       <QuestionCard question={question} />
-<<<<<<< HEAD
-
-      <h2 className="text-xl font-semibold">Your Answer</h2>
-      <AnswerForm questionId={id as string} userId={id} />
-=======
       <AnswerQuestion questionId={question.id} />
->>>>>>> 4863f1c2e102620673c533f5b2f37d3b384ccbe5
     </div>
   );
 }
