f5290d8fc4416501c604946d3f14bae52adf67b3
Udip <udipmandora42@gmail.com>
2025-03-19 00:10:39 -0400

    data capturing

--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -2,7 +2,7 @@ import QuestionCard from "@/components/Questions/QuestionCard";
 import { getQuestionById } from "@/use-cases/question-service";
 import { notFound } from "next/navigation";
 import AnswerQuestion from "./AnswerQuestion/AnswerQuestion";
-
+import { AnswerForm } from "@/app/new-question/answerForm";
 export default async function Page({
   params,
 }: {
@@ -16,7 +16,7 @@ export default async function Page({
   return (
     <div className="flex flex-col gap-2 w-full p-12">
       <QuestionCard question={question} />
-      <AnswerQuestion questionId={question.id} />
+      <AnswerQuestion questionId={question.id as string} />
     </div>
   );
 }
