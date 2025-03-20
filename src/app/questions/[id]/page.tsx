f5802213da659ad83a52922a3daa4e0a9b34c7d7
Udip <udipmandora42@gmail.com>
2025-03-20 14:39:28 -0400

    Made changes in form

--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -2,15 +2,11 @@ import QuestionCard from "@/components/Questions/QuestionCard";
 import { getQuestionById } from "@/use-cases/question-service";
 import { notFound } from "next/navigation";
 import AnswerQuestion from "./AnswerQuestion/AnswerQuestion";
-<<<<<<< HEAD
-import { AnswerForm } from "@/app/new-question/answerForm";
-=======
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { getAllAnswers } from "@/use-cases/answer-service";
 import AnswerList from "@/components/Answers/AnswerList";
 
->>>>>>> 72f1810232855ca87fc00ee61b9de1ede2ee7d5d
 export default async function Page({
   params,
 }: {
@@ -32,12 +28,8 @@ export default async function Page({
   return (
     <div className="flex flex-col gap-2 w-full p-12">
       <QuestionCard question={question} />
-<<<<<<< HEAD
-      <AnswerQuestion questionId={question.id as string} />
-=======
       <AnswerList answers={answers} />
       <AnswerQuestion questionId={question.id} userId={user.id} />
->>>>>>> 72f1810232855ca87fc00ee61b9de1ede2ee7d5d
     </div>
   );
 }
