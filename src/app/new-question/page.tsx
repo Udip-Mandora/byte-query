e78c7d4ed0e81114f8964c38adcc9f2d466f3301
Udip <udipmandora42@gmail.com>
2025-03-20 22:53:16 -0400

    fixed question form

--- src/app/new-question/page.tsx
+++ src/app/new-question/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { AskQuestionForm } from "./form";
 export default function Page() {
   return (
     <div className="mx-12 mt-6 flex flex-col gap-4">
       <span className="text-4xl font-bold">Ask your question.</span>
+      <AskQuestionForm></AskQuestionForm>
     </div>
   );
 }
